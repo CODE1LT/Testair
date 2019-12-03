42a85704f8797a934588bc0caa51a511e640d0ea
Aurelijus Suslavicius <a.suslavicius@ba.lt>
2019-12-03 22:58:59 +0200

    Some not important changes
    Signed-off-by: Aurelijus Suslavicius <a.suslavicius@ba.lt>

---
+++ app/src/androidTest/java/lt/code1/testair/CustomTestRunner.kt
@@ -0,0 +1,13 @@
+package lt.code1.testair
+
+//import android.app.Application
+//import android.content.Context
+//import androidx.test.runner.AndroidJUnitRunner
+//
+//@Suppress("unused")
+//class CustomTestRunner : AndroidJUnitRunner() {
+//
+//    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+//        return super.newApplication(cl, TestApplication::class.java.name, context)
+//    }
+//}
